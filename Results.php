02596e76a4a1b424e25327e919e1927e7ab70b8b
Alexandr Sokolov <64951280+allexandrsokollov@users.noreply.github.com>
2022-06-08 16:52:43 +0300

    Update Results.php

--- Results.php
+++ Results.php
@@ -1,19 +1,15 @@
 <?php
-$filename = __DIR__.'/trueAnswers';
-$array = file($filename);
-
-$keys = array("q0c", "q1a", "q2b", "q3c", "q4a", "q5a", "q6b", "q7a");
+$keys = array("q02", "q10", "q21", "q32", "q40", "q50", "q61", "q70");
 
 $numberOfRightAns = 0;
-for($num = 0; $num < count($array); $num++) {
+for($num = 0; $num < count($keys); $num++) {
     if (isset($_POST['q' . $num])){
         if($_POST['q'.$num] == $keys[$num]){
             $numberOfRightAns++;
         }
     }
 }
-$showResults = false;
-//Пробежать по всем радиокнопкам и посмотреть есть ли хоть один ответ, если есть, то показать результаты теста
+
 for($i = 0; $i < count($keys); $i++) {
     if(isset($_POST['q'.$i])) {
         $showResults = True;
